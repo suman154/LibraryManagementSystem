d58aa76e45d7995c7b40df53429112b8ed75599a
suman154 <sumanb@gmail.com>
2024-03-16 09:32:36 +0545

    Project Completed

--- book/urls.py
+++ book/urls.py
@@ -9,15 +9,14 @@ urlpatterns = [
     path('book-type/<int:pk>/',BookTypeView.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
     path('books/<int:pk>/', BooksEditView.as_view()),
     path('category/<int:pk>/',CategoryView.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
-    path('books/<int:pk>/', CategoryEditView.as_view()),
+    path('category/<int:pk>/', CategoryEditView.as_view()),
     path('authors/<int:pk>/',AuthorsView.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
-    path('books/<int:pk>/', AuthorsEditView.as_view()),
+    path('authors/<int:pk>/', AuthorsEditView.as_view()),
     path('add-book/<int:pk>/',AddBookView.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
     path('add-book/<int:pk>/', AddBookEditView.as_view()),
     path('register/',UserView.as_view({'post':'register'}),name='register'),
     path('login/',UserView.as_view({'post':'login'}),name='login'),
     path('user/',UserView.as_view({'post':'login'}),name='login'),
-
 
 
 ]
